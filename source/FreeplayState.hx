f3e45b9707c165d4c16baa1e543763185d694fb8
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2022-10-06 15:02:56 -0700

    ifofhgortor

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -64,7 +64,7 @@ class FreeplayState extends MusicBeatState
 		DiscordClient.changePresence("In the Menus", null);
 		#end
 
-		openfl.Lib.application.current.window.title = 'FNF: Psych Engine - In Freeplay'
+		openfl.Lib.application.current.window.title = 'FNF: Psych Engine - In Freeplay';
 
 		for (i in 0...WeekData.weeksList.length) {
 			if(weekIsLocked(WeekData.weeksList[i])) continue;
