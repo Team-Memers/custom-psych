222d43f03d707ad72a4c9970a80c0d9cfba4aeae
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2022-11-04 23:58:00 -0700

    kill me

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -79,7 +79,7 @@ class MainMenuState extends MusicBeatState
 		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
 
 		if(!ClientPrefs.darkMode)
-			bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'))
+			bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuBG'));
 		else 
 			bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('menuDesat'));
 
