72e3deec34fb0679d33332e0882ac890ea43bea0
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2022-11-21 13:30:40 -0800

    forgot that

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1205,7 +1205,7 @@ class PlayState extends MusicBeatState
 			difficultyWatermark.borderSize = 1.25;
 			add(difficultyWatermark);
 
-			playbackRateWatermark = new FlxText(11, difficultyWatermark.y + 25, 512, Std.string(playbackRate), 22);
+			playbackRateWatermark = new FlxText(11, difficultyWatermark.y + 25, 512, Std.string(playbackRate) + 'x Speed', 22);
 			playbackRateWatermark.setFormat(Paths.font('vcr.ttf'), 20, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 			playbackRateWatermark.scrollFactor.set();
 			playbackRateWatermark.borderSize = 1.25;
