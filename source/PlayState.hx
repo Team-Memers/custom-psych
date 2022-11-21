143cdeb31812aa62345e4b1c410f6cb0bebc3b15
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2022-11-20 22:30:45 -0800

    blank stage (i'll figure out what to do with this later)

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -513,6 +513,8 @@ class PlayState extends MusicBeatState
 
 		switch (curStage)
 		{
+			case 'blank':
+				//nothing lol
 			case 'stage': //Week 1
 				var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
 				add(bg);
