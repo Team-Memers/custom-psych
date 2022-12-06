c892bf681b04f1a1bfbc668ca719a44c4e3ff192
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2022-12-05 22:10:00 -0800

    dsdsdsdsd

--- source/Main.hx
+++ source/Main.hx
@@ -19,7 +19,7 @@ import openfl.display.StageScaleMode;
 import openfl.events.UncaughtErrorEvent;
 import haxe.CallStack;
 import haxe.io.Path;
-import Discord.DiscordClient;
+import funkin.utility.Discord.DiscordClient;
 import sys.FileSystem;
 import sys.io.File;
 import sys.io.Process;
