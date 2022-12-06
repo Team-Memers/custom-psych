9fced7e27ee2c9361c8a2f98047ca87616e0bb1b
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2022-12-05 21:56:40 -0800

    hghgfdhfdbfbgbgfdhfdbfd

--- source/Main.hx
+++ source/Main.hx
@@ -1,4 +1,4 @@
-package funkin;
+package;
 
 import flixel.graphics.FlxGraphic;
 import flixel.FlxG;
@@ -13,6 +13,7 @@ import openfl.display.FPS;
 import openfl.display.Sprite;
 import openfl.events.Event;
 import openfl.display.StageScaleMode;
+import funkin.utility.Preferences;
 
 //crash handler stuff
 #if CRASH_HANDLER
@@ -31,7 +32,7 @@ class Main extends Sprite
 {
 	var gameWidth:Int = 1280; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 720; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
-	var initialState:Class<FlxState> = TitleMenu; // The FlxState the game starts with.
+	var initialState:Class<FlxState> = funkin.states.menus.TitleMenu; // The FlxState the game starts with.
 	var zoom:Float = -1; // If -1, zoom is automatically calculated to fit the window dimensions.
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
